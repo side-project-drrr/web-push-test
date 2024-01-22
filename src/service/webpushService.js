d6b76720773e74b6a1f849da77beb919140a1a56
YoungKyonYou <man.spride1324@gmail.com>
2024-01-22 10:33:49 +0900

    typo fix

--- src/service/webpushService.js
+++ src/service/webpushService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const token = `eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJURUFNLVNBSURBLUJFIiwic3ViIjoiNTI0IiwiaWQiOjUyNCwiZXhwIjoxNjk4NTU0OTc0LCJpYXQiOjE2OTc5NTAxNzR9.jHhtp3qQCaNwLR-H72eUzYJnrXwBBTeRHJZdZFT_viT_GRclPYyFMH4h1pIU41IWEskX0JSY2hqD6cyf64iKc1u8V761Kj9FyIBfwMK-1pKLuGV2tjaWcviW5_euRahSO5uDYwiNMk6lovsIkwl8TdDryrhX-6lrHmx9YkRBbwTqYTZqRYjd8V7_p2tFGZXy6UJj444EpEjFkWHbu6_J_v7kq_NcVy44DAByPjId63LRnY08J0Tu2kcl6m_UO5X3rV3CEMs5ezcm_3wVdr1EA8RAkHIXUhC7W96ahFwPqGCzyafXsIsWvCPXYhjI5aboIQWhesHuVuBvY0DRzcitgA`;
+const token = `eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJEUlJSLUJFIiwic3ViIjoiNTAxIiwiaWQiOjUwMSwiZXhwIjoxNzA2NDkxMDg5LCJpYXQiOjE3MDU4ODYyODl9.m4zv3hTvaWA-VbXPsp02fK6frZ4XgVAN8M8xSV28qQrH1X0Xq4Z2KXaLT5IG-TUqjRgcyMf1cWfKrrT56zhEORynIHPURjWnyijlaQyYfLXcaafT8pLKpajzNpmfaenEoYcuRH_SNXuX3OrRQ0xflbjZvQKR6abU862ifIwQetSSRE1YByabvDpiyt2Ug2UzdSsxgIixBMd3FYYE2FrfBAyhx_ZkyDAA-9Gh0Watk75TS_oFVmenhQzFSeibMbdlwixYhtDInCXmQ6SfJTL250csgZAWsXrlv1Xq4BM7GYzFOzj7-8DY4zKbXys_jpqhdgeBpCf66pDD3hTjKl2tQQ`;
 
 export const pushService = (subscription) => {
   const jsonString = subscription.toJSON();
   const res = axios.post(
-    `http://localhost:8081/api/subscription`,
+    `http://localhost:8081/api/v1/member/subscription`,
     {
-      endpont: `${jsonString.endpoint}`,
+      endpoint: `${jsonString.endpoint}`,
       expirationTime: `${jsonString.expirationTime}`,
       p256dh: `${jsonString.keys.p256dh}`,
       auth: `${jsonString.keys.auth}`,
